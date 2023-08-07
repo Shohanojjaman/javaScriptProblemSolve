2e043d065478a8b6517043a359f46df30f7e8949
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2023-08-07 10:36:46 +0600

    Solved the problem which is determine whether a given year is a leap year in the Gregorian calendar.
    Problem:- Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.

---
+++ JavaScript basic/leapYear.js
@@ -0,0 +1,17 @@
+/*
+    Problem:
+    Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.  
+*/
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isLeapYear(2100));
