c0d26e6b8a6ee21357a7609ba0558d65434b4c81
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2023-08-07 16:51:14 +0600

    Update the solve.

--- JavaScript basic/randomInteger.js
+++ JavaScript basic/randomInteger.js
@@ -3,12 +3,13 @@
  Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched".  
  */
 
-function randomInteger(inputNumber) {
+function randomInteger(UserInput) {
+  let number = Number(UserInput);
   let random = Math.round(Math.random() * 10);
-  if (random === inputNumber) {
+  if (4 === number) {
     return 'Good Work';
   } else {
     return 'Not matched';
   }
 }
-console.log(randomInteger(4));
+console.log(randomInteger('4'));
